e90eb569b69295792860f566e7a3e666499ccaa9
Diego Matté <diegocmatte@gmail.com>
2021-08-22 03:33:50 -0300

    primeiro commit

---
+++ test/teste.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class teste {
+    @Test
+    public void testSomething() {
+
+        int a = 1 + 1;
+
+        assertEquals(2,a);
+
+    }
+}
